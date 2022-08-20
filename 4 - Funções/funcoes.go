c3405ba6d9d2ed37a2acdf2ccff3fe841e9a11bd
wagner-de-carvalho <wagncarv@outlook.com>
2022-08-20 18:57:20 -0300

    Funções retorno múltiplo

---
+++ 4 - Funções/funcoes.go
@@ -0,0 +1,27 @@
+package main
+import ("fmt")
+func main() {
+	soma := somar(10, 4)
+	fmt.Println(soma)
+
+	var f = func(txt string) string {
+		fmt.Println(txt)
+		return txt
+	}
+
+	f("SAPO")
+
+	resultSoma, resultSub := calculosMatematicos(20, 8)
+	fmt.Println(resultSoma)
+	fmt.Println(resultSub)
+}
+
+func calculosMatematicos(n1, n2 int8) (int8, int8) {
+	soma := n1 + n2
+	sub := n1 - n2
+	return soma, sub
+}
+
+func somar(n1 int8, n2 int8) int8 { 
+	return n1 + n2
+}
