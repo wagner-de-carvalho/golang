7e334c7e34f03c8224f672858eda2dd213212476
wagner <wagncarv@outlook.com>
2022-08-25 17:05:13 -0300

    Estruturas de controle parte 2

--- 11 - Estruturas de controle/estruturas-de-controle.go
+++ 11 - Estruturas de controle/estruturas-de-controle.go
@@ -4,10 +4,32 @@ import(
 	"fmt"
 )
 
+func diaDaSemana(numero int) string {
+	switch numero {
+		case 1:
+			return "Domingo"
+		case 2:
+			return "Segunda-feira"
+		case 3:
+			return "Terça-feira"
+		case 4:
+			return "Quarta-feira"
+		case 5:
+			return "Quinta-feira"
+		case 6:
+			return "Sexta-feira"
+		case 7:
+			return "Sábado"
+		default:
+			return "Número inválido"
+	}
+}
+
 func main() {
 	fmt.Println("Estruturas de controle")
 	numero := 10
 
+	// IF ELSE
 	if numero > 10 {
 		fmt.Println("Maior que 10")
 		}else {
@@ -19,5 +41,8 @@ func main() {
 	} else {
 		fmt.Println("Menor que zero")
 	}
+
+	// SWITCH
+	fmt.Println(diaDaSemana(6))
 
 }
