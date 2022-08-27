79a52c4cdacf94a1ac41bd45a774934fcae80d22
wagner <wagncarv@outlook.com>
2022-08-27 18:37:11 -0300

    Função buscarIps

--- 24 - Aplicação de Linha de Comando/app/app.go
+++ 24 - Aplicação de Linha de Comando/app/app.go
@@ -1,11 +1,42 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"net"
+	"log"
+	"github.com/urfave/cli"
+)
 
 //Retorna aplicação
 func Gerar() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca IPs e nome de servidores na internet"
+
+	app.Commands = []cli.Command{
+		{
+			Name: "ip",
+			Usage: "Busca IPs de endereços na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
 	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
